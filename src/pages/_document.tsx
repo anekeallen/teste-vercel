1c97592fd4141b4854a1d3735cc734259eba65a6
Aneke Allen <ti@ae.digital>
2023-05-12 17:21:32 -0300

    teste

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
+import Header from '@/components/Header'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body>
+        <Header></Header>
         <Main />
         <NextScript />
       </body>
---
+++ src/pages/sobre-nos.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header"
+
+export default function Sobre() {
+  return (
+    <>
+      {/* <Header></Header> */}
+      <h1>SOBRE NÓS</h1>
+    </>
+  )
+}
